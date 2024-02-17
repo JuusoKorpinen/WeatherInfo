ce2cc43630e8316fd87a2e382350d7408616099a
JuusoKorpinen <122262588+JuusoKorpinen@users.noreply.github.com>
2024-02-17 20:37:28 +0200

    Done

---
+++ components/Weather.js
@@ -0,0 +1,56 @@
+import { View, Text, Image, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+
+const api = {
+    url: 'https://api.openweathermap.org/data/2.5/weather?',
+    key: process.env.EXPO_PUBLIC_API_KEY,
+    icons: 'http://openweathermap.org/img/wn/'
+}
+
+export default function Weather(props) {
+    const [temp, setTemp] = useState(0)
+    const [description, setDescription] = useState('')
+    const [icon, setIcon] = useState('')
+
+    useEffect(() => {
+        const url = api.url +
+            'lat=' + props.latitude +
+            '&lon=' + props.longitude +
+            '&units=metric' +
+            '&appid=' + api.key
+
+        fetch(url)
+            .then(res => res.json())
+            .then((json) => {
+                console.log(json)
+                setTemp(json.main.temp)
+                setDescription(json.weather[0].description)
+                setIcon(api.icons + json.weather[0].icon + '@2x.png')
+            })
+            .catch((error) => {
+                setDescription('Error retriving weather information.')
+                console.log(error)
+            })
+
+    }, [])
+
+
+    return (
+        <View>
+            <Text style={styles.temp}>{temp}</Text>
+            {icon &&
+                <Image source={{ uri: icon }} style={{ width: 100, height: 100 }} />
+            }
+            <Text style={styles.description}>{description}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    temp: {
+        margin: 8
+    },
+    description: {
+        margin: 8
+    }
+});
